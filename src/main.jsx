155e0c6ac42d38ecef1a48a98a85d93b3a29c1a6
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-19 11:43:55 +0600

    Project Setup

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./Components/AuthProvider/AuthProvider.jsx";
+import router from "./Router";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
